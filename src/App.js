b625ef395e8f71b6d8597fe57635e542b6ccf4e7
farhadctg22201~ <farhadctg222@gmail.com>
2023-08-12 11:58:03 +0600

    second comiit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componet/Header/Header';
 
 function App() {
+  const studernt =[{
+    name: 'farhad',
+    age: 20,
+    classname:'eight'
+  },{
+    name:'rokib',
+    age:30,
+    classname:'nine'
+  }]
   return (
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Header></Header>
+        
+        {/* {
+          studernt.map(id=><Header student={id} key={id.age}></Header>)
+        } */}
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/componet/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+//    const [name,age]= props.student
+    return (
+        <div>
+            <h1>hell world</h1>
+            <h4>this praktice</h4>
+        </div>
+    );
+};
+
+export default Header;
